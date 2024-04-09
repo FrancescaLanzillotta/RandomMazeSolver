48e5fed05543121bd5afad047357e698f02ef6c9
Francesca <francesca.lanzillotta@stud.unifi.it>
2024-04-09 14:02:27 +0200

    Simplified initializeMaze implementation

--- Maze.h
+++ Maze.h
@@ -28,8 +28,6 @@ private:
     int size;
 public:
     explicit Maze(int size);
-    static vector<Cell> buildRowWall(int length);
-    vector<Cell> buildCyclicWall(int length);
     string toString();
     void initializeMaze();  // TODO change to private
     void set_exit(int row, int col);
