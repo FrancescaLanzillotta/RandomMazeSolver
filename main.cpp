3cc00ef64805c058de515fd483f1937b3c5beb0d
Francesca <francesca.lanzillotta@stud.unifi.it>
2024-04-14 11:10:47 +0200

    Added particles loop

--- main.cpp
+++ main.cpp
@@ -6,56 +6,34 @@ using namespace std;
 
 int main() {
 
-    int size = 15;
+    int size = 21;
     random_device rd;  // a seed source for the random number engine
     mt19937 rng(rd()); // mersenne_twister_engine seeded with rd()
     uniform_int_distribution<> distrib(0, size - 1);
 
 
-
-
     Maze m(size, rng);
     m.generateMaze(false);
     cout << m.toString();
-    Particle p(m);
 
-    while (p.getPosition() != m.getExit()){
-        p.randMove();
-        delayedCLS(1);
-        cout << m.toString();
+    vector<Particle> particles;
+    int nParticles = 15;
+    for (int i = 0; i < nParticles; ++i) {
+        particles.emplace_back(m);
+
     }
-
-
-
-
-
-
-
-
-
-    /*
-    uniform_int_distribution<> dir(0, 4);
-    for (int i = 0; i < 5; ++i) {
-        auto d = static_cast<Direction>(dir(rng));
-        switch (d) {
-            case UP:
-                cout << "UP" << endl;
+    bool out = false;
+    while (!out){
+        for(auto& p : particles){
+            p.randMove(false);
+            delayedCLS(1);
+            cout << m.toString();
+            if (p.getPosition() == m.getExit()){
+                out = true;
                 break;
-            case DOWN:
-                cout << "DOWN" << endl;
-                break;
-            case LEFT:
-                cout << "LEFT" << endl;
-                break;
-            case RIGHT:
-                cout << "RIGHT" << endl;
-                break;
-            case STAY:
-                cout << "STAY" << endl;
-                break;
+            }
         }
     }
-    */
     return 0;
 
 }
